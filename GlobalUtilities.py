02cb7c766bd29d62eb7bf3817ad050fd227ec63c
Ryan Hefner <rhefner.1@gmail.com>
2013-02-01 23:50:04 -0500

    Fixed typo in reading contacts_json property from Settings object

--- GlobalUtilities.py
+++ GlobalUtilities.py
@@ -739,7 +739,7 @@ class SettingsData(UtilitiesBase):
         memcache_key = "contacts" + self.e.websafe
 
         def get_item():
-            return self.e.contact_json
+            return self.e.contacts_json
 
         return cache(memcache_key, get_item)
 
