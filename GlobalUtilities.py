de8c48621820ea8f5fcf357c6c3f3c2673354adc
Ryan Hefner <rhefner.1@gmail.com>
2013-02-12 17:54:48 -0500

    Fixed issue affecting return_all in search results

--- GlobalUtilities.py
+++ GlobalUtilities.py
@@ -384,7 +384,7 @@ def searchReturnAll(query, search_results, settings, search_function, entity_ret
             all_results.extend(searchToDocuments(results))
 
         query_cursor = search_results.cursor
-        results = search_function(query, query_cursor=query_cursor, entity_return=entity_return)
+        results = search_function(query, query_cursor=query_cursor, entity_return=entity_return)[0]
 
     return all_results
 
