13170a9924d80b8ed940e959a2e92613585cd5b5
Ryan Hefner <rhefner1@gmail.com>
2013-11-07 12:46:08 -0500

    Readded endpoints cookies monkey patch

--- ghiendpoints.py
+++ ghiendpoints.py
@@ -11,6 +11,21 @@ import endpoints
 from protorpc import remote
 from endpoints_messages import *
 
+## Cloud Endpoints Cookies - monkey patch
+from endpoints import api_config
+
+class PatchedApiConfigGenerator(api_config.ApiConfigGenerator):
+  def pretty_print_config_to_json(self, services, hostname=None):
+    json_string = super(PatchedApiConfigGenerator, self).pretty_print_config_to_json(
+        services, hostname=hostname)
+    to_patch = json.loads(json_string)
+    to_patch['auth'] = {'allowCookieAuth': True}
+    return json.dumps(to_patch, sort_keys=True, indent=2)
+ 
+api_config.ApiConfigGenerator = PatchedApiConfigGenerator
+
+## End monkey patch
+
 endpoints_client_id = "AIzaSyB7k0LsUXibTJHkCx_D3MA0HT6tQAtYZAo"
 endpoints_description = "GHI Donations API"
 endpoints_clients = [endpoints_client_id, endpoints.API_EXPLORER_CLIENT_ID]
