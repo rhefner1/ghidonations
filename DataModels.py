1a2a23998a7c1f5f3b4a0cb1595102a79d068e71
Ryan Hefner <rhefner1@gmail.com>
2013-11-07 12:40:36 -0500

    Remove extra conflict header

--- DataModels.py
+++ DataModels.py
@@ -647,13 +647,8 @@ class Settings(ndb.Expando):
         if donor_report_text != s.donor_report_text:
             s.donor_report_text = donor_report_text
 
-<<<<<<< HEAD
         s.put()
-           
-=======
-        s.put()       
-
->>>>>>> issue-52
+              
     @property
     def websafe(self):
         return self.key.urlsafe()
